8e9159b228a2124983566024edc478b4d0aabc6a
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-03-04 09:15:13 -0500

    Add files via upload

---
+++ src/ejrciciosEntregables/metodosJava.java
@@ -0,0 +1,26 @@
+package ejerciciosEstructurasDeControl;
+
+
+import javax.swing.*;
+
+public class metodosJava {
+    public static int pedirNumeroPositivo(String mensaje) {
+        int numero;
+        while (true) {
+            try {
+                String input = JOptionPane.showInputDialog(mensaje);
+
+                numero = Integer.parseInt(input);
+
+                if (numero >= 0) {
+                    return numero;
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se permiten números negativos. Inténtalo de nuevo.");
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada no válida. Ingresa solo números enteros.");
+            }
+        }
+    }
+}
+
