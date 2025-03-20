d29dc1604e6ea429deca1f1be8835db60b8a6cae
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-03-20 10:24:03 -0500

    Add files via upload

---
+++ src/estructurasDeDatos/Ejercicio7.java
@@ -0,0 +1,20 @@
+package estructurasDeDatos;
+
+import javax.swing.*;
+
+public class Ejercicio7 {
+    public static void main(String[] args){
+        int[] numbers;
+        int i=0;
+
+
+        do{
+            numbers[*]=JOptionPane.showInputDialog("Ingrese el dato entero");
+            i++;
+
+        }while(JOptionPane.showConfirmDialog(null, "¿Desea agregar otro dato?", "Repetir", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION);
+
+
+
+    }
+}
