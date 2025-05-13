4e3e518ed9b7751900182d16152913d849f44a88
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-05-13 11:30:37 -0500

    Update Procesos.java

--- src/POO/EjercicioModeloDeDatos/logica/Procesos.java
+++ src/POO/EjercicioModeloDeDatos/logica/Procesos.java
@@ -1,15 +1,20 @@
 package POO.EjercicioModeloDeDatos.logica;
 
-public class Procesos {
+    public class Procesos {
         double por=0;
         public void calcularSueldo(Operador operador){
             double aumento=0;
             if(operador.getSueldo()<500&&operador.getAntiguedad()>=10){
                 por=0.2;
-                operador.setAumento(por*100);
-                aumento= operador.getSueldo()+(operador.getSueldo()*por);
-                operador.setSueldoNuevo(aumento);
+            }else if(operador.getSueldo()<500&&operador.getAntiguedad()<10){
+                por=0.05;
+            }else if(operador.getSueldo()>=500){
+                por=0.0;
+
             }
+            operador.setAumento(por*100);
+            aumento= operador.getSueldo()+(operador.getSueldo()*por);
+            operador.setSueldoNuevo(aumento);
             ;
         }
     }
