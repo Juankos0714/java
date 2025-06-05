3c22ee04aa80eb951f522bcc621c25ece77da481
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-06-05 10:31:39 -0500

    Update PersonaVo.java

--- src/TrabajoComboBox/vo/PersonaVo.java
+++ src/TrabajoComboBox/vo/PersonaVo.java
@@ -4,14 +4,28 @@ public class PersonaVo {
     private String documento;
     private String nombre;
     private int edad;
+    private String direccion;
 
     public PersonaVo() {
     }
 
-    public PersonaVo(String documento, String nombre, int edad) {
+    public PersonaVo(String s, String s1, int i) {
+    }
+
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public PersonaVo(String documento, String nombre, int edad, String direccion) {
         this.documento = documento;
         this.nombre = nombre;
         this.edad = edad;
+        this.direccion= direccion;
     }
 
     public String getDocumento() {
@@ -46,3 +60,4 @@ public class PersonaVo {
         return nombre + " - " + documento;
     }
 }
+
